1097e01c375a1fca4f3a020ffa8da24d86d12bb5
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2023-02-09 18:14:06 +0100

    Make the site work as a static site (don't use any SSR for redirection)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,65 @@
-import { GetStaticProps } from "next";
+import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { getArticleSlugs } from "lib/get-articles-slugs";
+import { PaginatedArticles } from "components/paginated-articles";
 
-const Index = () => {
-  return null;
+const ARTICLES_PER_PAGE = 4;
+
+const Page = ({ slugs }) => {
+  const router = useRouter();
+  const page = parseInt(router.query.page as string);
+
+  const Articles = slugs.map((slug) => dynamic(() => import(`../changelogs/${slug}.mdx`)));
+
+  return (
+    <PaginatedArticles page={page}>
+      {Articles.map((Article, index) => (
+        <Article key={index} hideLayout={true} hideHead={true} hideAuthors={true} />
+      ))}
+    </PaginatedArticles>
+  );
 };
 
-export const getStaticProps: GetStaticProps = async (ctx) => {
+export async function getStaticPaths() {
+  const slugs = getArticleSlugs();
+  const articlesLength = Math.floor(slugs.length / ARTICLES_PER_PAGE);
+  const numbers = Array.from(Array(articlesLength), (x, i) => i);
+
   return {
-    redirect: {
-      destination: "/page/0",
-      permanent: true,
-    },
+    paths: numbers.map((number) => ({
+      params: {
+        page: number.toString(),
+      },
+    })),
+    fallback: false,
   };
-};
+}
 
-export default Index;
+export async function getStaticProps() {
+  const slugs = getArticleSlugs();
+
+  const results = await Promise.allSettled(
+    slugs.map((slug) => import(`../changelogs/${slug}.mdx`))
+  );
+
+  const meta = results
+    .map((res) => res.status === "fulfilled" && res.value.meta)
+    .filter((item) => item);
+
+  meta.sort((a, b) => {
+    const dateB = new Date(b.publishedAt);
+    const dateA = new Date(a.publishedAt);
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  const start = 0;
+  const end = ARTICLES_PER_PAGE;
+  const recents = meta.slice(start, end).map((item) => item.slug);
+
+  return {
+    props: { slugs: recents },
+    revalidate: 1,
+  };
+}
+
+export default Page;
