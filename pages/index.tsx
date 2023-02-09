5278b8461661155a064a69fd3ea7f315e34318f4
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2023-02-09 18:46:03 +0100

    Replace client side re-routing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,65 @@
-import { useEffect } from "react";
 import { useRouter } from "next/router";
+import dynamic from "next/dynamic";
+import { getArticleSlugs } from "lib/get-articles-slugs";
+import { PaginatedArticles } from "components/paginated-articles";
 
-const Index = () => {
+const ARTICLES_PER_PAGE = 4;
+
+const Page = ({ slugs }) => {
   const router = useRouter();
+  const page = parseInt(router.query.page as string);
 
-  useEffect(() => {
-    router.replace("/page/0");
-  }, [router]);
+  const Articles = slugs.map((slug) => dynamic(() => import(`../changelogs/${slug}.mdx`)));
 
-  return null;
+  return (
+    <PaginatedArticles page={page}>
+      {Articles.map((Article, index) => (
+        <Article key={index} hideLayout={true} hideHead={true} hideAuthors={true} />
+      ))}
+    </PaginatedArticles>
+  );
 };
-export default Index;
+
+export async function getStaticPaths() {
+  const slugs = getArticleSlugs();
+  const articlesLength = Math.floor(slugs.length / ARTICLES_PER_PAGE);
+  const numbers = Array.from(Array(articlesLength), (x, i) => i);
+
+  return {
+    paths: numbers.map((number) => ({
+      params: {
+        page: number.toString(),
+      },
+    })),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps() {
+  const slugs = getArticleSlugs();
+
+  const results = await Promise.allSettled(
+    slugs.map((slug) => import(`../changelogs/${slug}.mdx`))
+  );
+
+  const meta = results
+    .map((res) => res.status === "fulfilled" && res.value.meta)
+    .filter((item) => item);
+
+  meta.sort((a, b) => {
+    const dateB = new Date(b.publishedAt);
+    const dateA = new Date(a.publishedAt);
+    return dateB.getTime() - dateA.getTime();
+  });
+
+  const start = 0;
+  const end = ARTICLES_PER_PAGE;
+  const recents = meta.slice(start, end).map((item) => item.slug);
+
+  return {
+    props: { slugs: recents },
+    revalidate: 1,
+  };
+}
+
+export default Page;
