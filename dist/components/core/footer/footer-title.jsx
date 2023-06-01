e887198114827d9568e00b3c8655d1e2d9cde092
Tami <tammilore@gmail.com>
2023-06-01 16:23:53 +0100

    deleted dist

--- dist/components/core/footer/footer-title.jsx
+++
@@ -1,7 +0,0 @@
-import { Text } from "@chakra-ui/react";
-import React from "react";
-export function FooterTitle(props) {
-    return (<Text fontFamily="landingHeading" fontWeight="bold" color="landing.black">
-      {props.children}
-    </Text>);
-}
