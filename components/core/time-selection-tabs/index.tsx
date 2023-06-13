e524f3f2999eb902e29b324cc8a6135f4ae2814c
Jethro Tanjay <tanjay.jethro@gmail.com>
2023-06-13 11:20:04 +0800

    update switcher UI

--- components/core/time-selection-tabs/index.tsx
+++ components/core/time-selection-tabs/index.tsx
@@ -1,16 +1,11 @@
-import {
-  Button,
-  VStack,
-  ButtonGroup,
-} from "@chakra-ui/react";
+import { Button, VStack, ButtonGroup } from "@chakra-ui/react";
 import { motion } from "framer-motion";
 import useTimelineStore from "lib/state/useTimelineStore";
-
 
 const TimeSelectionTabs = () => {
   const buttonGroup: Array<"weeks" | "months" | "years"> = ["weeks", "months", "years"];
 
-  const timeline = useTimelineStore()
+  const timeline = useTimelineStore();
   return (
     <VStack>
       <ButtonGroup
@@ -20,24 +15,25 @@ const TimeSelectionTabs = () => {
         borderRadius="100px"
         padding="5px"
         color="#868E96"
-        border="3px solid rgba(104, 104, 247, 0.25)"
+        h="46px"
+        width="268px"
       >
         {buttonGroup.map((view, index) => (
           <Button
             key={view}
-            h="36px"
             borderRadius="100px"
+            height="full"
             border="0px"
             fontWeight="bold"
             fontSize="14px"
             lineHeight="20px"
             verticalAlign="middle"
-            px={5}
+            width="full"
             style={
               timeline.view === view
                 ? {
                     background: "transparent",
-                    color: "#FFFFFF",
+                    color: "#0D131B",
                   }
                 : {}
             }
@@ -56,7 +52,7 @@ const TimeSelectionTabs = () => {
                 style={{
                   position: "absolute",
                   inset: 0,
-                  background: "linear-gradient(180deg, #6868F7 0%, #4C40D9 100%)",
+                  background: "#FFFFFF",
                   borderRadius: "9999px",
                   boxShadow: "0px 0.636364px 2.24px rgba(33, 40, 54, 0.16)",
                 }}
