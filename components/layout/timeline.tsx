2f43d9f4e5499c4926982be039a6faa692dcaf6e
Jethro Tanjay <tanjay.jethro@gmail.com>
2023-06-23 10:57:22 +0800

    fix responsiveness bug, change back button

--- components/layout/timeline.tsx
+++ components/layout/timeline.tsx
@@ -1,6 +1,6 @@
 import { Box, HStack, Text, VStack, useMediaQuery } from "@chakra-ui/react";
 import { useRouter } from "next/router";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export interface TimelineProps {
   date: string;
@@ -13,7 +13,10 @@ const Timeline = (props: TimelineProps) => {
   const router = useRouter();
   const [isLargerThan768] = useMediaQuery("(min-width: 768px)");
 
-  const selected = router.pathname.includes("/changelogs");
+  const [isOpen, setIsOpen] = useState(false);
+  useEffect(() => {
+    setIsOpen(router.pathname.includes("/changelogs"));
+  }, [router.pathname]);
 
   return (
     <HStack
@@ -22,62 +25,76 @@ const Timeline = (props: TimelineProps) => {
       justifyContent="center"
       alignItems="start"
       spacing={0}
-      pt={selected ? (isLargerThan768 ? 52 : 28) : 0}
-      px={selected ? [6, 6, 0] : 0}
+      pt={isOpen ? (isLargerThan768 ? 52 : 28) : 0}
+      px={isOpen ? [6, 6, 0] : 0}
     >
-      {/* {isLargerThan768 &&( */}
-      <VStack
-        position="relative"
-        top={selected ? "" : "-8px"}
-        width="120px"
-        spacing={4}
-        display={isLargerThan768 ? "flex" : "none"}
-      >
-        {selected && (
-          <Box
-            onClick={() => {
-              router.back();
-            }}
-            cursor="pointer"
-            display="flex"
-            gap={3}
-            alignItems="center"
-            justifyContent="start"
-            width="125px"
-          >
-            <div>
+      {isLargerThan768 && (
+        <VStack
+          position="relative"
+          top={isOpen ? "" : "-8px"}
+          width="120px"
+          spacing={4}
+        >
+          {isOpen && (
+            <Box
+              onClick={() => {
+                router.back();
+              }}
+              cursor="pointer"
+              display="flex"
+              gap={3}
+              alignItems="center"
+              justifyContent="start"
+              width="125px"
+            >
               <svg
-                width="6"
+                width="11"
                 height="10"
-                viewBox="0 0 6 10"
+                viewBox="0 0 11 10"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path
-                  d="M5.25 9L1.25 5L5.25 1"
-                  stroke="#868E96"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
+                <g clip-path="url(#clip0_2_2540)">
+                  <path
+                    d="M1.25 5.75L10.25 5.75C10.6642 5.75 11 5.41421 11 5C11 4.58579 10.6642 4.25 10.25 4.25L1.25 4.25C0.835787 4.25 0.5 4.58579 0.5 5C0.5 5.41421 0.835787 5.75 1.25 5.75Z"
+                    fill="black"
+                  />
+                  <path
+                    d="M5.25 9L1.25 5L5.25 1"
+                    stroke="black"
+                    strokeWidth="1.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </g>
+                <defs>
+                  <clipPath id="clip0_2_2540">
+                    <rect
+                      width="11"
+                      height="10"
+                      fill="white"
+                      transform="translate(11 10) rotate(-180)"
+                    />
+                  </clipPath>
+                </defs>
               </svg>
-            </div>
-            <Text
-              fontSize="16px"
-              color="#868E96"
-              alignItems="start"
-              _hover={{ textDecoration: "underline" }}
-            >
-              Back
-            </Text>
-          </Box>
-        )}
-        <Text fontSize="16px" color="#868E96" alignItems="start" width="125px">
-          {date}
-        </Text>
-      </VStack>
-      {/* )} */}
-      <HStack alignItems="start" spacing={selected ? 0 : 8} display="relative">
+
+              <Text
+                fontSize="16px"
+                color="#0D131B"
+                alignItems="start"
+                _hover={{ textDecoration: "underline" }}
+              >
+                Back
+              </Text>
+            </Box>
+          )}
+          <Text fontSize="16px" color="#868E96" alignItems="start" width="125px">
+            {date}
+          </Text>
+        </VStack>
+      )}
+      <HStack alignItems="start" spacing={isOpen ? 0 : 8} display="relative">
         <Box
           style={{
             display: "flex",
@@ -87,7 +104,7 @@ const Timeline = (props: TimelineProps) => {
             width: "10px",
             position: "absolute",
           }}
-          hidden={selected}
+          hidden={isOpen}
         >
           <Box
             style={{
@@ -109,55 +126,72 @@ const Timeline = (props: TimelineProps) => {
           />
         </Box>
         <VStack alignItems="start" spacing={[0, 0, 2]}>
-          <VStack
-            position="relative"
-            top="-8px"
-            spacing={4}
-            display={isLargerThan768 ? "none" : "flex"}
-            mb={[4, 4]}
-          >
-            {selected && (
-              <Box
-                onClick={() => {
-                  router.back();
-                }}
-                cursor="pointer"
-                display="flex"
-                gap={3}
-                alignItems="center"
-                justifyContent="start"
-                width="full"
-              >
-                <svg
-                  width="6"
-                  height="10"
-                  viewBox="0 0 6 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+          {!isLargerThan768 && (
+            <VStack
+              position="relative"
+              top="-8px"
+              spacing={4}
+              mb={[4, 4]}
+            >
+              {isOpen && (
+                <Box
+                  onClick={() => {
+                    router.back();
+                  }}
+                  cursor="pointer"
+                  display="flex"
+                  gap={3}
+                  alignItems="center"
+                  justifyContent="start"
+                  width="full"
                 >
-                  <path
-                    d="M5.25 9L1.25 5L5.25 1"
-                    stroke="#868E96"
-                    strokeWidth="1.5"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
+                  <svg
+                    width="11"
+                    height="10"
+                    viewBox="0 0 11 10"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <g clip-path="url(#clip0_2_2540)">
+                      <path
+                        d="M1.25 5.75L10.25 5.75C10.6642 5.75 11 5.41421 11 5C11 4.58579 10.6642 4.25 10.25 4.25L1.25 4.25C0.835787 4.25 0.5 4.58579 0.5 5C0.5 5.41421 0.835787 5.75 1.25 5.75Z"
+                        fill="black"
+                      />
+                      <path
+                        d="M5.25 9L1.25 5L5.25 1"
+                        stroke="black"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
+                    </g>
+                    <defs>
+                      <clipPath id="clip0_2_2540">
+                        <rect
+                          width="11"
+                          height="10"
+                          fill="white"
+                          transform="translate(11 10) rotate(-180)"
+                        />
+                      </clipPath>
+                    </defs>
+                  </svg>
 
-                <Text
-                  fontSize="16px"
-                  color="#868E96"
-                  alignItems="start"
-                  _hover={{ textDecoration: "underline" }}
-                >
-                  Back
-                </Text>
-              </Box>
-            )}
-            <Text fontSize="16px" color="#868E96" alignItems="start" width="full">
-              {date}
-            </Text>
-          </VStack>
+                  <Text
+                    fontSize="16px"
+                    color="#0D131B"
+                    alignItems="start"
+                    _hover={{ textDecoration: "underline" }}
+                  >
+                    Back
+                  </Text>
+                </Box>
+              )}
+              <Text fontSize="16px" color="#868E96" alignItems="start" width="full">
+                {date}
+              </Text>
+            </VStack>
+          )}
           {children}
         </VStack>
       </HStack>
