c598f85c613867e70418966e57ea1c31b545e1e2
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2023-08-29 15:19:55 +0200

    Add code to manage changelog assets in s3

---
+++ sync_images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Source the .env file to get BUCKET_NAME
+source ./.env
+
+# Check if BUCKET_NAME is set
+if [[ -z "$BUCKET_NAME" ]]; then
+    echo "Error: BUCKET_NAME not set in .env file."
+    exit 1
+fi
+
+if [ ! "$(ls -A ./changelog-images)" ]; then
+    echo "The changelog-images directory is empty or does not exist."
+    # Add any additional actions you want to perform in this scenario
+    exit 1
+fi
+
+# Sync changelog-images to the S3 bucket
+aws s3 sync ./changelog-images s3://$BUCKET_NAME/ --acl public-read --exclude '*/.DS_Store' --delete
