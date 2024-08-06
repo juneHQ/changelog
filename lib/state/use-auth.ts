895495a86f582177ea6f25a02201b310f9bd4f9a
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-08-06 13:21:59 +0200

    Send cors mode request header

--- lib/state/use-auth.ts
+++ lib/state/use-auth.ts
@@ -9,16 +9,13 @@ export const useAuth = () => {
   useEffect(() => {
     const checkLoginStatus = async () => {
       try {
-        console.log('Checking login status');
-        console.log(JSON.stringify(process.env));
         const response = await fetch(`${process.env.NEXT_PUBLIC_JUNE_API_HOST}/logged_in`, {
           credentials: 'include',
-          mode: 'no-cors',
+          mode: 'cors',
         });
         const data = await response.json();
         setLoggedIn(data.loggedIn);
       } catch (error) {
-        console.error('Error checking login status:', error);
         setLoggedIn(false);
       }
     };
